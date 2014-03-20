c80627ec15f10b36c32c87cce1cb532ec08dcc4f
vinnugaa <vinnugaa@gmail.com>
2014-03-20 23:34:43 +0530

    Commiting Market action and service classes with country name

---
+++ RequestTracker/src/com/tracker/action/MarketAction.java
@@ -0,0 +1,69 @@
+package com.tracker.action;
+
+import com.tracker.delegate.BusinessDelegate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.tracker.constants.AllConstants;
+import com.tracker.data.DataResult;
+import com.tracker.delegate.BusinessDelegate;
+import com.tracker.model.DC;
+import com.tracker.utils.Utility;
+
+public class MarketAction {
+	
+	private String id;
+	private String countryName;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getcountryName() {
+		return countryName;
+	}
+	public void setcountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	
+	
+	BusinessDelegate bd = new BusinessDelegate();
+	HttpServletRequest request = ServletActionContext.getRequest();
+	HttpSession session = request.getSession();
+	
+	public boolean add(Market countryName){
+		DataResult<String> dataResult = null;
+		
+		bd.setServiceInput(AllConstants.SERVICE_MAIN_MARKET, AllConstants.SERVICE_METHOD_ADD, Utility.getInstance().toJson(dc));
+		dataResult = bd.doTask();
+		return dataResult.isSuccess;
+	}
+	public boolean delete(Market countryName){
+		DataResult<String> dataResult = null;
+		
+		bd.setServiceInput(AllConstants.SERVICE_MAIN_MARKET, AllConstants.SERVICE_METHOD_DELETE, Utility.getInstance().toJson(dc));
+		dataResult = bd.doTask();
+		return dataResult.isSuccess;
+	}
+	public boolean update(Market countryName){
+		DataResult<String> dataResult = null;
+		
+		bd.setServiceInput(AllConstants.SERVICE_MAIN_MARKET, AllConstants.SERVICE_METHOD_UPDATE, Utility.getInstance().toJson(dc));
+		dataResult = bd.doTask();
+		return dataResult.isSuccess;
+	}
+	public String getAll(){
+		DataResult<String> dataResult = null;
+		
+		bd.setServiceInput(AllConstants.SERVICE_MAIN_MARKET, AllConstants.SERVICE_METHOD_ALL, AllConstants.EMPTY_STRING);
+		dataResult = bd.doTask();
+		return dataResult.data;
+	}
+
+}
---
+++ RequestTracker/src/com/tracker/service/MarketService.java
@@ -0,0 +1,75 @@
+package com.tracker.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.tracker.cache.TrackerCache;
+import com.tracker.constants.AllConstants;
+import com.tracker.data.DataResult;
+import com.tracker.database.DcDAO;
+import com.tracker.model.DC;
+import com.tracker.utils.Utility;
+
+
+public class MarketService extends BusinessService {
+	
+	public MarketService(String method, String inputJson) {
+		super(method, inputJson);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public DataResult<String> doProcessing() {
+		// TODO Auto-generated method stub
+		DataResult<String> dataResult= null;
+		DC srccountryName=null, destcountryName=null;
+		List<countryName> allcountries = null;
+		MarketDAO marketDAO = new MarketDAO();
+		
+		srccountryName = (marketDAO)(new Gson()).fromJson(inputJson, Market.class);
+		
+		if(method.equalsIgnoreCase(AllConstants.SERVICE_METHOD_EACH)){
+			destcountryName =  marketDAO.getEach(srccountryName);
+			if(destcountryName != null){
+				dataResult = new DataResult<String>();
+				dataResult.isSuccess=true;
+				dataResult.data = Utility.getInstance().toJson(destcountryName);
+			}
+		}else if(method.equalsIgnoreCase(AllConstants.SERVICE_METHOD_ADD)){
+			marketDAO.add(srccountryName);
+			dataResult = new DataResult<String>();
+			dataResult.isSuccess=true;
+			//dataResult.data = Utility.getInstance().toJson(destcountryName);
+			
+		}else if(method.equalsIgnoreCase(AllConstants.SERVICE_METHOD_DELETE)){
+			marketDAO.delete(srccountryName);
+			dataResult = new DataResult<String>();
+			dataResult.isSuccess=true;
+			
+		}else if(method.equalsIgnoreCase(AllConstants.SERVICE_METHOD_UPDATE)){
+			marketDAO.update(srccountryName);
+			dataResult = new DataResult<String>();
+			dataResult.isSuccess=true;
+			
+		}else if(method.equalsIgnoreCase(AllConstants.SERVICE_METHOD_ALL)){
+			//CHECK THE CACHE FIRST
+			allcountries = (ArrayList<marketDAO>)TrackerCache.getInstance().getObject(AllConstants.CACHE_MARKET_ALL);
+			if (allcountries != null) {
+				/*dataResult = new DataResult<String>();
+				dataResult.data = Utility.getInstance().toJson(allcountries);
+				dataResult.isSuccess=true;*/
+			}else {
+				allcountries = marketDAO.getAll();
+				TrackerCache.getInstance().addObject(AllConstants.CACHE_DC_ALL, allcountries);
+			}
+			dataResult = new DataResult<String>();
+			dataResult.data = Utility.getInstance().toJson(allcountries);
+			dataResult.isSuccess=true;
+			
+		}
+		
+		return dataResult;
+	}
+
+}
